78be95251fd548016b0b6d98d821a7a766a011ec
yunus <yyas404@gmail.com>
2024-01-10 00:30:33 +0530

    cicd test commit

--- src/frameworks/express/router/userRouter.ts
+++ src/frameworks/express/router/userRouter.ts
@@ -4,4 +4,5 @@ import signup from '../../../adapters/controllers/user/userRegisterController'
 
 export const userRoute=express.Router()
 
+userRoute.get('/',(req,res)=>res.send("hello world"))
 userRoute.post("/signup",signup.signup)
